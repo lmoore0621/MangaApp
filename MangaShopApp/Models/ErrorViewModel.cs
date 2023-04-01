47150f9164be25b51b271e1a870239534e4f2453
Leon Moore <leon@sharphue.com>
2023-04-01 15:20:06 -0500

    Add project files.

---
+++ MangaShopApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MangaShopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
